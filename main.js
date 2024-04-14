67e19195ea91e3a7f8a9db35f05bd1e1e4b1b8d8
Your Name <you@example.com>
2024-04-14 14:57:37 -0300

    Changes of the html and javascript

--- main.js
+++ main.js
@@ -16,15 +16,13 @@ for (let i = 0; i < buttons.length; i++) {
 }
 
 const counters = document.querySelectorAll(".counter");
-const time = new Date("2023-04-05T00:00:00");
+const time = new Date("2024-04-30T00:00:00");
 const time2 = new Date("2024-05-05T00:00:00");
 const time3 = new Date("2024-06-05T00:00:00");
 const time4 = new Date("2024-06-25T00:00:00");
 
 const times = [time, time2, time3, time4];
 
-
-// counters[0].textContent = calculateTime(time);
 
 function calculateTime(ObjcetiveTime) {
     let courrentTime = new Date();
@@ -48,13 +46,16 @@ function calculateTime(ObjcetiveTime) {
 
 function updateTimer() {
     //pega os ids do index.html,
-    document.getElementById("days0").textContent = calculateTime(times[1])[0];
-    document.getElementById("hours0").textContent = calculateTime(times[1])[1];
-    document.getElementById("minutes0").textContent = calculateTime(times[1])[2];
-    document.getElementById("seconds0").textContent = calculateTime(times[1])[3];
-
+    // document.getElementById("days0").textContent = calculateTime(times[0])[0];
+    // document.getElementById("hours0").textContent = calculateTime(times[0])[1];
+    // document.getElementById("minutes0").textContent = calculateTime(times[0])[2];
+    // document.getElementById("seconds0").textContent = calculateTime(times[0])[3];
     for (let i = 0; i < counters.length; i++) {
-        // counters[i].textContent = calculateTime(times[i]);
+        document.getElementById("days" + i).textContent = calculateTime(times[i])[0];
+        document.getElementById("hours" + i).textContent = calculateTime(times[i])[1];
+        document.getElementById("minutes" + i).textContent = calculateTime(times[i])[2];
+        document.getElementById("seconds" + i).textContent = calculateTime(times[i])[3];
+        console.log('seg' + i);
     }
 }
 
